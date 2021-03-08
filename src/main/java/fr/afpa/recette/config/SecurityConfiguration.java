bae4891a16d04cd54c52592deb6ed338dd6e81d9
Développeur web <51367556+Francois-Legrand@users.noreply.github.com>
2021-03-08 18:05:53 +0100

    add api/account.authenticated

--- src/main/java/fr/afpa/recette/config/SecurityConfiguration.java
+++ src/main/java/fr/afpa/recette/config/SecurityConfiguration.java
@@ -82,6 +82,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
+            .antMatchers("/api/account").authenticated()
             .antMatchers("/api/**").permitAll()
             .antMatchers("/websocket/tracker").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/websocket/**").permitAll()
